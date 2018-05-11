78b0888893f725c2ca57bfbc82751b1a7a88f314
xsrong <xsrong2@163.com>
2018-05-11 13:26:21 +0800

    Update Stack.rb

--- Basic Data Structure/Stack.rb
+++ Basic Data Structure/Stack.rb
@@ -5,18 +5,18 @@ require_relative 'Node'
 
 class Stack
   def initialize            # 新建一个栈
-    @first = nil
+    @first = nil            # @first表示处理时会被第一个处理的元素。即栈尾的元素。
     @l = 0
   end
 
-  def push item             # 向一个栈的尾部添加一个元素
+  def push item             # 向栈的尾部添加一个元素，给@fisrt赋予新的值，并将@first.next设为原来的@first
     old_first = @first
     @first = Node.new item
     @first.next = old_first
     @l += 1
   end
   
-  def pop                   # 删除栈尾部的元素
+  def pop                   # 删除栈尾部的元素。即把最后一个元素指定为原来倒数第二的那个元素，这样原最后一个元素就成了孤儿，会被回收。
     return nil if @first == nil
     item = @first.item
     @first = @first.next
